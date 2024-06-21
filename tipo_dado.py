d89ac708906b7bdfe2a4c9f00e653145dd77a3c4
F4biano <167382107+Sw1cky@users.noreply.github.com>
2024-06-21 18:52:38 -0300

    Add files via upload

---
+++ tipo_dado.py
@@ -0,0 +1,8 @@
+d1 = 5
+d2 = 2 
+print(type(d1))
+print(type(d2))
+
+dividido = d1 / d2 
+print(dividido)
+print(type(dividido))
---
+++ tipo_dado2.py
@@ -0,0 +1,11 @@
+t1 = "o resultado é"
+d1 = 10
+d2 = 3.5 
+
+soma = d1 + d2 
+
+print(t1, soma) 
+print(type(t1))
+print(type(d1))
+print(type(d2))
+print(type(soma))
